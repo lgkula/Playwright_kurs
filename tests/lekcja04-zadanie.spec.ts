8fa26a780e3c549385d3bc18cc139f45f1efc4f7
Leslaw Kula <leslaw.kula@capgemini.com>
2023-09-09 00:11:43 +0200

    update homework

--- tests/lekcja04-zadanie.spec.ts
+++ tests/lekcja04-zadanie.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test.describe('Logowanie do banku test', () => {
     test('', async ({ page }) => {
         await page.goto('https://demo-bank.vercel.app/');
-        await expect(page).toHaveTitle('/Logowanie/')
+        await expect(page).toHaveTitle(/Logowanie/);
         await page.getByTestId('login-input').fill('12345678');
         await page.getByTestId('password-input').fill('haslo123')
         await expect(page.getByTestId('login-button')).toBeEnabled;
@@ -19,7 +19,12 @@ test('Doładuje telefon o 100zł test', async ({ page }) => {
     await page.locator('#widget_1_topup_amount').fill('100');
     await page.locator('#widget_1_topup_agreement').check();
     await page.locator('#execute_phone_btn').click();
-
+    await expect(page.locator('#ui-id-1')).toHaveText('Doładowanie wykonane');
+    await expect(page.locator('#i-info')).toContainText(' Doładowanie wykonane!');
+    await expect(page.locator('#i-info')).toContainText('100,00PLN');
+    await expect(page.locator('#i-info')).toContainText('502 xxx xxx');
+    await page.getByTestId('close-button').click();
+    await expect(page.getByTestId('message-text')).toHaveText('Doładowanie wykonane! 55,00PLN na numer 502 xxx xxx');
 })
 
 
