1a5f318e5991f2746bb256c2186d62ec0ceab84c
Leslaw Kula <leslaw.kula@capgemini.com>
2023-09-08 23:57:38 +0200

    Next lessons and homework

--- tests/lekcja01.spec.ts
+++ tests/lekcja01.spec.ts
@@ -1,15 +1,15 @@
 import { test, expect } from "@playwright/test";
 
-// test("Login with correct credentials", async ({ page }) => {
-//   await page.goto("https://demo-bank.vercel.app/");
-//   await page.getByTestId("login-input").click();
-//   await page.getByTestId("login-input").fill("testerLO");
-//   await page.getByTestId("password-input").click();
-//   await page.getByTestId("password-input").fill("10987654");
-//   await page.getByTestId("login-button").click();
-//   await page.getByTestId("user-name").click();
+test("Login with correct credentials", async ({ page }) => {
+  await page.goto("https://demo-bank.vercel.app/");
+  await page.getByTestId("login-input").click();
+  await page.getByTestId("login-input").fill("testerLO");
+  await page.getByTestId("password-input").click();
+  await page.getByTestId("password-input").fill("10987654");
+  await page.getByTestId("login-button").click();
+  await page.getByTestId("user-name").click();
 
-//   await expect(page.getByTestId("user-name")).toHaveText("Jan Demobankowy");
-// });
+  await expect(page.getByTestId("user-name")).toHaveText("Jan Demobankowy");
+});
 
-// test("xxx", async ({ page }) => {});
+test("xxx", async ({ page }) => {});
