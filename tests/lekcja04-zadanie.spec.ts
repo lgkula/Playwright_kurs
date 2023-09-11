6969c616fdb330d02c9385f5c51b6346c806bd48
Leszek <leslawkula@gmail.com>
2023-09-12 01:31:03 +0200

    Update homework test

--- tests/lekcja04-zadanie.spec.ts
+++ tests/lekcja04-zadanie.spec.ts
@@ -12,6 +12,21 @@ test.describe('Test funkcjonalności doładowania w banku', () => {
         await expect(page).toHaveTitle('Demobank - Bankowość Internetowa - Pulpit')
     })
 
+test.only('Doładuje telefon o 50zł test', async ({ page }) => {
+    await expect(page).toHaveTitle('Demobank - Bankowość Internetowa - Pulpit')
+    await page.locator('#widget_1_topup_receiver').selectOption('502 xxx xxx');
+    await expect(page.locator('#uniform-widget_1_topup_receiver > span')).toHaveText('502 xxx xxx');
+    await page.locator('#widget_1_topup_amount').fill('50');
+    await page.locator('#widget_1_topup_agreement').check();
+    await page.locator('#execute_phone_btn').click();
+    await expect(page.locator('#ui-id-1')).toHaveText('Doładowanie wykonane');
+    // await expect(page.locator('#i-info')).toContainText(' Doładowanie wykonane!');
+    // await expect(page.locator('#i-info')).toContainText('50,00PLN');
+    // await expect(page.locator('#i-info')).toContainText('502 xxx xxx');
+    await page.getByTestId('close-button').click();
+    await expect(page.getByTestId('message-text')).toHaveText('Doładowanie wykonane! 50,00PLN na numer 502 xxx xxx');
+})
+
 test.only('Doładuje telefon o 100zł test', async ({ page }) => {
     await expect(page).toHaveTitle('Demobank - Bankowość Internetowa - Pulpit')
     await page.locator('#widget_1_topup_receiver').selectOption('502 xxx xxx');
@@ -25,37 +40,20 @@ test.only('Doładuje telefon o 100zł test', async ({ page }) => {
     // await expect(page.locator('#i-info')).toContainText('502 xxx xxx');
     await page.getByTestId('close-button').click();
     await expect(page.getByTestId('message-text')).toHaveText('Doładowanie wykonane! 100,00PLN na numer 502 xxx xxx');
-})
-
-test.only('Doładuje telefon o 222zł test', async ({ page }) => {
-    await expect(page).toHaveTitle('Demobank - Bankowość Internetowa - Pulpit')
-    await page.locator('#widget_1_topup_receiver').selectOption('502 xxx xxx');
-    await expect(page.locator('#uniform-widget_1_topup_receiver > span')).toHaveText('502 xxx xxx');
-    await page.locator('#widget_1_topup_amount').fill('222');
-    await page.locator('#widget_1_topup_agreement').check();
-    await page.locator('#execute_phone_btn').click();
-    await expect(page.locator('#ui-id-1')).toHaveText('Doładowanie wykonane');
-    // await expect(page.locator('#i-info')).toContainText(' Doładowanie wykonane!');
-    // await expect(page.locator('#i-info')).toContainText('100,00PLN');
-    // await expect(page.locator('#i-info')).toContainText('502 xxx xxx');
-    await page.getByTestId('close-button').click();
-    await expect(page.getByTestId('message-text')).toHaveText('Doładowanie wykonane! 222,00PLN na numer 502 xxx xxx');
     })
     
-test.only('Doładuje telefon o 333zł test', async ({ page }) => {
+test.only('Doładuje telefon o 100zł test bez akceptacji regulaminu', async ({ page }) => {
     await expect(page).toHaveTitle('Demobank - Bankowość Internetowa - Pulpit')
     await page.locator('#widget_1_topup_receiver').selectOption('502 xxx xxx');
     await expect(page.locator('#uniform-widget_1_topup_receiver > span')).toHaveText('502 xxx xxx');
-    await page.locator('#widget_1_topup_amount').fill('333');
-    await page.locator('#widget_1_topup_agreement').check();
+    await page.locator('#widget_1_topup_amount').fill('100');
+    // await page.locator('#widget_1_topup_agreement').check();
     await page.locator('#execute_phone_btn').click();
     await expect(page.locator('#ui-id-1')).toHaveText('Doładowanie wykonane');
     // await expect(page.locator('#i-info')).toContainText(' Doładowanie wykonane!');
     // await expect(page.locator('#i-info')).toContainText('100,00PLN');
     // await expect(page.locator('#i-info')).toContainText('502 xxx xxx');
     await page.getByTestId('close-button').click();
-    await expect(page.getByTestId('message-text')).toHaveText('Doładowanie wykonane! 333,00PLN na numer 502 xxx xxx');
-})
-
-
+    await expect(page.getByTestId('message-text')).toHaveText('Doładowanie wykonane! 100,00PLN na numer 502 xxx xxx');
+    })
 })
