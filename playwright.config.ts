fbaf4af2e51ea7abc8524167772383af5b209dab
Leslaw Kula <leslaw.kula@capgemini.com>
2023-09-21 20:48:20 +0200

    Test data files

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,6 +34,7 @@ export default defineConfig({
   },
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'https://demo-bank.vercel.app',
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
--- tests/lekcja12.spec.ts
+++ tests/lekcja12.spec.ts
@@ -1,89 +1,71 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe('Pulpit tests', () => {
+test.describe("Pulpit tests", () => {
   test.beforeEach(async ({ page }) => {
-    const userId = 'testerLO';
-    const userPassword = '10987654';
+    const userId = "testerLO";
+    const userPassword = "10987654";
 
-    await page.goto('/');
-    await page.getByTestId('login-input').fill(userId);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
+    await page.goto("/");
+    await page.getByTestId("login-input").fill(userId);
+    await page.getByTestId("password-input").fill(userPassword);
+    await page.getByTestId("login-button").click();
   });
 
-  test('quick payment with correct data', async ({ page }) => {
+  test("quick payment with correct data", async ({ page }) => {
     // Arrange
-    const receiverId = '2';
-    const transferAmount = '150';
-    const transferTitle = 'pizza';
-    const expectedTransferReceiver = 'Chuck Demobankowy';
+    const receiverId = "2";
+    const transferAmount = "150";
+    const transferTitle = "pizza";
+    const expectedTransferReceiver = "Chuck Demobankowy";
 
     // Act
-    await page.locator('#widget_1_transfer_receiver').selectOption(receiverId);
-    await page.locator('#widget_1_transfer_amount').fill(transferAmount);
-    await page.locator('#widget_1_transfer_title').fill(transferTitle);
+    await page.locator("#widget_1_transfer_receiver").selectOption(receiverId);
+    await page.locator("#widget_1_transfer_amount").fill(transferAmount);
+    await page.locator("#widget_1_transfer_title").fill(transferTitle);
 
-    await page.getByRole('button', { name: 'wykonaj' }).click();
-    await page.getByTestId('close-button').click();
+    await page.getByRole("button", { name: "wykonaj" }).click();
+    await page.getByTestId("close-button").click();
 
     // Assert
-    await expect(page.locator('#show_messages')).toHaveText(
+    await expect(page.locator("#show_messages")).toHaveText(
       `Przelew wykonany! ${expectedTransferReceiver} - ${transferAmount},00PLN - ${transferTitle}`
     );
   });
 
-  test('successful mobile top-up', async ({ page }) => {
+  test("successful mobile top-up", async ({ page }) => {
     // Arrange
-    const topUpReceiver = '500 xxx xxx';
-    const topUpAmount = '50';
+    const topUpReceiver = "500 xxx xxx";
+    const topUpAmount = "50";
     const expectedMessage = `Doładowanie wykonane! ${topUpAmount},00PLN na numer ${topUpReceiver}`;
 
     // Act
-    await page.locator('#widget_1_topup_receiver').selectOption(topUpReceiver);
-    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
-    await page.locator('#uniform-widget_1_topup_agreement span').click();
-    await page.getByRole('button', { name: 'doładuj telefon' }).click();
-    await page.getByTestId('close-button').click();
+    await page.locator("#widget_1_topup_receiver").selectOption(topUpReceiver);
+    await page.locator("#widget_1_topup_amount").fill(topUpAmount);
+    await page.locator("#uniform-widget_1_topup_agreement span").click();
+    await page.getByRole("button", { name: "doładuj telefon" }).click();
+    await page.getByTestId("close-button").click();
 
     // Assert
-    await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
+    await expect(page.locator("#show_messages")).toHaveText(expectedMessage);
   });
 
-  test('correct balance after successful mobile top-up', async ({ page }) => {
+  test("correct balance after successful mobile top-up", async ({ page }) => {
     // Arrange
-    const topUpReceiver = '500 xxx xxx';
-    const topUpAmount = '50';
-    const initialBalance = await page.locator('#money_value').innerText();
+    const topUpReceiver = "500 xxx xxx";
+    const topUpAmount = "50";
+    const initialBalance = await page.locator("#money_value").innerText();
     const expectedBalance = Number(initialBalance) - Number(topUpAmount);
 
     // Act
-    await page.locator('#widget_1_topup_receiver').selectOption(topUpReceiver);
-    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
-    await page.locator('#uniform-widget_1_topup_agreement span').click();
-    await page.getByRole('button', { name: 'doładuj telefon' }).click();
-    await page.getByTestId('close-button').click();
+    await page.locator("#widget_1_topup_receiver").selectOption(topUpReceiver);
+    await page.locator("#widget_1_topup_amount").fill(topUpAmount);
+    await page.locator("#uniform-widget_1_topup_agreement span").click();
+    await page.getByRole("button", { name: "doładuj telefon" }).click();
+    await page.getByTestId("close-button").click();
 
     // Assert
-    await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
+    await expect(page.locator("#money_value")).toHaveText(`${expectedBalance}`);
   });
+
   
-  test('correct1 balance after successful mobile top-up', async ({ page }) => {
-    // Arrange
-    const transferAmount = '50';
-    // wartość którą wprowadzimy na stronie np. kwota przelewu
-    const initialBalance = await page.locator('#selektor').innerText();
-    // pobiera element ze strony i wyciąga z niego wartość, np. saldo przed przelem
-    const expectedBalance = Number(initialBalance) - Number(transferAmount);
-    // wylicza oczekiwaną wartość np. saldo po transakcji
-    // elementy pobrane ze strony to zawsze String, więc potrzeba je przerobić na Number
-
-    // Act
-    // ...
-    // np. kroki wykonujące tranzakcję
-
-    // Assert
-    await expect(page.locator('#selektor')).toHaveText(`${expectedBalance}`);
-    // asercja sprawdza czy oczekiwana wartość zmiennej jest prawidłowa
-    // asercja używa Stringów więc Numer trzeba przekonwertować na String
-  });
 });
